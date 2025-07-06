49863127b481249f8489153eac1705a190887851
Lea Caramanos <108407366+lea-caramanos@users.noreply.github.com>
2025-07-06 11:42:01 +0100

    Extracting API logic to its own folder.

--- api/pokeapi.py
+++ api/pokeapi.py
@@ -1,8 +1,9 @@
 import random
 import requests
 
+# Get pokemon's info via the API
 def get_random_pokemon_api():
     pokemon_id = random.randint(1, 151)
-    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}/'
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
     response = requests.get(url)
     return response.json()
